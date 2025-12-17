d58bf0bd0a520630c307406a60ac34ddfbf1980a
Natalia Matioshko <165213062+NatalinaMatioshko@users.noreply.github.com>
2025-12-18 01:14:49 +0200

    add scroll

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigationType } from "react-router-dom";
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
+  const navType = useNavigationType(); // "PUSH" | "POP" | "REPLACE"
 
   useEffect(() => {
+    if (navType === "POP") return; // Back/Forward -> зберігаємо scroll
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [pathname, navType]);
 
   return null;
 }
